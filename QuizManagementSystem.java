150a597bb6ffa5d92192912df96c3b1e5d3c9c1e
Rommel <karlsrommel255@gmail.com>
2023-11-18 20:22:21 -0600

    practicaDeJava0004

---
+++ QuizManagementSystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class QuizManagementSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la clave de la prueba:");
+        String quizCode = scanner.next();
+
+        // Aquí debes implementar la lógica para iniciar la prueba
+        // Esto podría implicar cargar las preguntas de la base de datos
+        System.out.println("La prueba ha comenzado.");
+    }
+}
